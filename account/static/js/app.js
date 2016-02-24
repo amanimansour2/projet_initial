1fdb8710815f9e16f331de831f2e449926d32d72
amani <amani.mansour2@gmail.com>
2016-02-24 16:11:50 +0100

    test reset data.username works

--- account/static/js/app.js
+++ account/static/js/app.js
@@ -1,12 +1,17 @@
 (function(){
 var app= angular.module('account', []);
-app.controller('accountcontroller', ['$scope', function($scope) {
+app.controller('accountcontroller', ['$scope','$http', function($scope,$http) {
     $scope.input = "entrez le nom du processus";
     $scope.name = 'voici le pid';
 
-    $scope.getpid = function(name) {
-    $scope.name = "123456789";
-    };
+    $scope.getpid = function() {
+     
+        $http({
+       method : "GET",
+       url : "/account/users/1/"
+       }).then(function (response) {
+         $scope.name = response.data.username;
+     });}
 }]);
 })();
 
